e5087a43a01b2d0e11fa7dcbb17af4ec2148bdec
Alex Johny <a.johny87@gmail.com>
2025-05-18 23:23:24 -0300

    new commit

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -1,10 +1,44 @@
-class CardNews extends HTMLElement {
+class CardNews extends HTMLElement{
     constructor() {
         super();
 
-        const shadow = this.attachShadow({mode: "open"});
-        shadow.innerHTML = "<h1>Rebeca Amor da minha Vida Te Amo</h1>";
+        const shadow = this.attachShadow({ mode: "open"});
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.style());
     }
+
+    build() {
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
+
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card__left");
+        const autor = document.createElement("span");
+        autor.textContent = "By " + (this.getAttribute("autor") || "Anonymous");
+
+        const linkTitle = document.createElement("a");
+        linkTitle.textContent = this.getAttribute("title");
+
+        const newsContent = document.createElement("p");
+        newsContent.textContent = this.getAttribute("content");
+
+        cardLeft.appendChild(autor);
+        cardLeft.appendChild(linkTitle);
+        cardLeft.appendChild(newsContent);
+
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "card__right");
+        const newsImage = document.createElement("img");
+
+        cardRight.appendChild(newsImage);
+
+        componentRoot.appendChild(cardLeft);    
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot;
+    }
+
+    style() {}
 }
 
-customElements.define('card-news',CardNews);
+customElements.define("card-news", CardNews);
