664f66cf2acf0b0a0ea312d4672f1fcf3568d84b
Alex Johny <a.johny87@gmail.com>
2025-05-19 23:40:40 -0300

    prps

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -18,6 +18,7 @@ class CardNews extends HTMLElement{
 
         const linkTitle = document.createElement("a");
         linkTitle.textContent = this.getAttribute("title");
+        linkTitle.href = this.getAttribute("link-url");
 
         const newsContent = document.createElement("p");
         newsContent.textContent = this.getAttribute("content");
@@ -29,6 +30,8 @@ class CardNews extends HTMLElement{
         const cardRight = document.createElement("div");
         cardRight.setAttribute("class", "card__right");
         const newsImage = document.createElement("img");
+        newsImage.src="assets/asset-vader.png"
+        newsImage.alt = "Foto da Noticia"
 
         cardRight.appendChild(newsImage);
 
@@ -38,7 +41,45 @@ class CardNews extends HTMLElement{
         return componentRoot;
     }
 
-    style() {}
+    style() {
+        const style = document.createElement("style");
+        style.textContent = `
+        .card{
+    width: 80%;
+    border: 1px solid gray;
+    box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
+    -webkit-box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
+    -moz-box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+
+.card__left{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding-left: 10px;
+}
+
+.card__left > span {
+    font-weight: 400;
+}
+
+.card__left > a {
+    margin-top: 15px;
+    font-size: 25px;
+    color: black;
+    text-decoration: none;
+    font-weight: bold;
+}
+
+.card__left > p {
+    color: rgb(102, 99, 99);
+}
+        `;
+        return style;
+    }
 }
 
 customElements.define("card-news", CardNews);
